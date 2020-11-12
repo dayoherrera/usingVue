2a3d5ef0be3558268823014c8c2ddc8e9fb91efe
Dayona Herrera <dayona79@gmail.com>
2020-11-11 20:38:08 -0400

    usando v-for

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ new Vue({
         return {
             name: 'Bitcoin',
             img: 'https://claveprivada.com/wp-content/uploads/2018/10/1024px-Bitcoin.svg-800x800.png',
-            changePercent: -1
+            changePercent: -1,
+            prices:  [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miercoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sabado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ]
         }
     }
 })
