173bd0b87c61980163aa8bb66ef749d761b5a54b
Dayona Herrera <dayona79@gmail.com>
2020-12-02 11:21:19 -0400

    enviando informacion del componente padre al componente hijo con un objeto

--- app.js
+++ app.js
@@ -1,50 +1,79 @@
+//using components
+Vue.component('coin-detail', 
+    {   
+        props: ['coin'],
+        data(){
+            return {
+                showPrices: false,
+                value: 0,
+            }
+        },
+        template: `
+        <div>
+            <img v-on:mouseover="toggleShowPrices()" v-on:mouseout="toggleShowPrices()" v-bind:src="coin.img" v-bind:alt="coin.name" width="45px">
+
+            <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
+            {{ title }}
+            <span v-if="coin.changePercent > 0">👍</span>
+            <span v-else-if="changePercent < 0">👎</span>
+            <span v-else>🤞</span>
+            <span v-on:click="toggleShowPrices()">{{ showPrices ? '🙉' : '🙈' }}</span>
+            </h1>
+
+            <input type="number" v-model="value">
+            <span>{{convertedValue}}</span>
+
+            <ul v-show=showPrices>
+                <li 
+                v-bind:class="{ orange: p.value == coin.price, red: p.value < coin.price, green: p.value > coin.price}" v-for="(p, i) in coin.pricesWithDays" v-bind:key="p.value">{{i+1}} - {{p.day}} - {{p.value}} </li>
+            </ul>
+        </div>
+        `,
+        methods: {
+            toggleShowPrices(){
+                this.showPrices = !this.showPrices;
+            }
+        },
+        computed: { // propiedad computada
+    
+            convertedValue(){ // Two-Way Data Binding
+                if(!this.value){
+                    return 0;
+                }else{
+                    return (this.value /this.coin.price);
+                }
+            },
+            title(){
+                return `${this.coin.name} - ${this.coin.symbol}`
+            }
+        }
+    }
+);
+
 // Creando la instancia de Vue con new
 new Vue({
     el: '#app', // pasar por parámetro el objeto
 
     data () {
         return {
-            name: 'Bitcoin',
-            symbol: 'BTC',
-            value: 0, // propiedad para Two-Way Data Binding
-            img: 'https://claveprivada.com/wp-content/uploads/2018/10/1024px-Bitcoin.svg-800x800.png',
-            changePercent: -1,
-            price: 8200,
-            color: 'f4f4f4',
-            pricesWithDays: [
-                { day: 'Lunes', value: 8400 },
-                { day: 'Martes', value: 7900 },
-                { day: 'Miercoles', value: 8200 },
-                { day: 'Jueves', value: 9000 },
-                { day: 'Viernes', value: 9400 },
-                { day: 'Sabado', value: 10000 },
-                { day: 'Domingo', value: 10200 }
-            ],
-            showPrices: false
-        }
-    },
-    computed: { // propiedad computada
-        title(){
-            return `${this.name} - ${this.symbol}`
-        },
 
-        convertedValue(){ // Two-Way Data Binding
-            if(!this.value){
-                return 0;
-            }else{
-                return (this.value /this.price);
-            }
-        }
-    },
-    watch: { //watchers
-        showPrices(newVal, oldVal){
-            console.log(newVal, oldVal);
-        }
-    },
-    methods: {
-        toggleShowPrices(){
-            this.showPrices = !this.showPrices;
-            this.color = this.color.split('').reverse().join('');
+            btc:{
+                name: 'Bitcoin',
+                symbol: 'BTC',
+                img: 'https://claveprivada.com/wp-content/uploads/2018/10/1024px-Bitcoin.svg-800x800.png',
+                changePercent: -1,
+                price: 8200,
+                pricesWithDays: [
+                    { day: 'Lunes', value: 8400 },
+                    { day: 'Martes', value: 7900 },
+                    { day: 'Miercoles', value: 8200 },
+                    { day: 'Jueves', value: 9000 },
+                    { day: 'Viernes', value: 9400 },
+                    { day: 'Sabado', value: 10000 },
+                    { day: 'Domingo', value: 10200 }
+                ]
+            },
+            color: 'f4f4f4'
         }
     }
 })
