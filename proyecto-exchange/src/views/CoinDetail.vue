04e5fabfef266ca37f04192bef8d2b7dfae738f8
Dayona Herrera <dayona79@gmail.com>
2021-01-10 20:28:39 -0400

    usando chart js y vue spinner

--- proyecto-exchange/src/views/CoinDetail.vue
+++ proyecto-exchange/src/views/CoinDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <template v-if="!isLoading">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -63,6 +66,14 @@
           <span class="text-xl"></span>
         </div>
       </div>
+
+      <line-chart
+        class="my-10"
+        :colors="['orange']"
+        :min="min"
+        :max="max"
+        :data="chartData"
+      />
     </template>
   </div>
 </template>
@@ -75,6 +86,7 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       asset: {},
       history: [],
     }
@@ -85,6 +97,14 @@ export default {
       return Math.min(
         ...this.history.map((h) => parseFloat(h.priceUsd).toFixed(2))
       )
+    },
+
+    chartData() {
+      const data = []
+      this.history.map((h) => {
+        data.push([h.date, parseFloat(h.priceUsd).toFixed(2)])
+      })
+      return data
     },
 
     max() {
@@ -109,12 +129,14 @@ export default {
     getCoin() {
       const id = this.$route.params.id
 
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
+      this.isLoading = true
+
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
+        .then(([asset, history]) => {
           this.asset = asset
           this.history = history
-        }
-      )
+        })
+        .finally(() => (this.isLoading = false))
     },
   },
 }
