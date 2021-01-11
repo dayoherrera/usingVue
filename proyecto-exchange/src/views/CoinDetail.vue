dfd059849b4043c36f996156f268d82cd0f0f32e
Dayona Herrera <dayona79@gmail.com>
2021-01-11 12:10:20 -0400

    usando this.set para problemas de reactividad en vue 2

--- proyecto-exchange/src/views/CoinDetail.vue
+++ proyecto-exchange/src/views/CoinDetail.vue
@@ -7,7 +7,9 @@
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
-            :src="`https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`"
+            :src="
+              `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
+            "
             :alt="asset.name"
             class="w-20 h-20 mr-5"
           />
@@ -49,9 +51,7 @@
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Cambiar
-          </button>
+          >Cambiar</button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
@@ -72,53 +72,70 @@
         :colors="['orange']"
         :min="min"
         :max="max"
-        :data="chartData"
+        :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
       />
+
+      <h3 class="text-xl my-10">Mejores Ofertas de Cambio</h3>
+      <table>
+        <tr v-for="m in markets" :key="`${m.exchangeId}-${m.priceUsd}`" class="border-b">
+          <td>
+            <b>{{ m.exchangeId }}</b>
+          </td>
+          <td>{{ m.priceUsd | dollar }}</td>
+          <td>{{ m.baseSymbol }} / {{ m.quoteSymbol }}</td>
+          <td>
+            <px-button
+              :is-loading="m.isLoading || false"
+              v-if="!m.url"
+              @custom-click="getWebSite(m)"
+            >
+              <slot>Obtener Link</slot>
+            </px-button>
+
+            <a v-else class="hover:underline text-green-600" target="_blanck">{{ m.url }}</a>
+          </td>
+        </tr>
+      </table>
     </template>
   </div>
 </template>
 
 <script>
 import api from '@/api'
+import PxButton from '@/components/PxButton'
 
 export default {
   name: 'CoinDetail',
+
+  components: { PxButton },
 
   data() {
     return {
       isLoading: false,
       asset: {},
       history: [],
+      markets: []
     }
   },
 
   computed: {
     min() {
       return Math.min(
-        ...this.history.map((h) => parseFloat(h.priceUsd).toFixed(2))
+        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
       )
-    },
-
-    chartData() {
-      const data = []
-      this.history.map((h) => {
-        data.push([h.date, parseFloat(h.priceUsd).toFixed(2)])
-      })
-      return data
     },
 
     max() {
       return Math.max(
-        ...this.history.map((h) => parseFloat(h.priceUsd).toFixed(2))
+        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
       )
     },
 
     avg() {
-      return (
-        this.history.reduce((a, b) => a + parseFloat(b.priceUsd), 0) /
-        this.history.length
+      return Math.abs(
+        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
       )
-    },
+    }
   },
 
   created() {
@@ -126,19 +143,36 @@ export default {
   },
 
   methods: {
+    getWebSite(exchange) {
+      this.$set(exchange, 'isLoading', true)
+
+      return api
+        .getExchange(exchange.exchangeId)
+        .then(res => {
+          this.$set(exchange, 'url', res.exchangeUrl)
+        })
+        .finally(() => {
+          this.$set(exchange, 'isLoading', false)
+        })
+    },
+
     getCoin() {
       const id = this.$route.params.id
-
       this.isLoading = true
 
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
-        .then(([asset, history]) => {
+      Promise.all([
+        api.getAsset(id),
+        api.getAssetHistory(id),
+        api.getMarkets(id)
+      ])
+        .then(([asset, history, markets]) => {
           this.asset = asset
           this.history = history
+          this.markets = markets
         })
         .finally(() => (this.isLoading = false))
-    },
-  },
+    }
+  }
 }
 </script>
 
@@ -148,4 +182,3 @@ td {
   text-align: center;
 }
 </style>
-
